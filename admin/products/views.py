d5066d10549f78152583a61c91b3e93536a2c6c0
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2021-06-26 23:24:08 +0300

    API and user endpoint setup

---
+++ admin/products/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Product, User
+from .serializers import ProductSerializer 
+import random
+
+
+class ProductViewSet(viewsets.ViewSet):
+    # Get the list of the products
+    def get_list(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+
+        return Response(serializer.data)
+
+    # Create new product
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # Get a specific product
+    def retrieve(self, request, pk=None):
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(product)
+
+        return Response(serializer.data)
+
+    # Update the information of existed product
+    def update(self, request, pk=None):
+        product = Product.objects.get(id=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # Delete product from the db
+    def destroy(self, request, pk=None):
+        product = Product.objects.get(id=pk)
+        product.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserAPIView(APIView):
+    def get(self,_):
+        users = User.objects.get()
+        user = random.choice(users)
+
+        return Response({
+            'id': user.id
+        })
