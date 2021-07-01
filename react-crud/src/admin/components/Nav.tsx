189b8743ca061d66c3a677e5d8643976faa5c6fb
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2021-07-01 23:07:01 +0300

    crud app

--- react-crud/src/admin/components/Nav.tsx
+++ react-crud/src/admin/components/Nav.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Nav = () => {
     return (
         <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-            <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
+            <a className="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Products name</a>
             <button className="navbar-toggler position-absolute d-md-none collapsed" type="button"
                     data-toggle="collapse"
-                    data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+                    data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="true"
                     aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
--- react-crud/src/main/Main.tsx
+++ react-crud/src/main/Main.tsx
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Product} from "../interfaces/product";
 
 const Main = () => {
+    const [products, setProducts] = useState([] as Product[]);
+
+    useEffect( () => {
+      (
+        async () => {
+          const response = await fetch("http://localhost:8001/api/products");
+          const data = await response.json();
+
+          setProducts(data);
+        }
+      )();
+    }, []);
+
+    const like = async (id: number) =>{
+      await fetch(`http://localhost:8001/api/products/${id}/like`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'}
+      });
+
+      setProducts(products.map(
+        (p: Product) => {
+          if (p.id === id){
+            p.likes++;
+          }
+          return p;
+        }
+      ));
+    }
+
     return(
       <main role="main">
   
@@ -8,21 +38,29 @@ const Main = () => {
           <div className="container">
   
             <div className="row">
-              <div className="col-md-4">
-                <div className="card mb-4 box-shadow">
-                  <div className="card-body">
-                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <div className="d-flex justify-content-between align-items-center">
-                      <div className="btn-group">
-                        <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                        <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+            
+            {products.map(
+                (p: Product) => {
+                  return (
+                    <div className="col-md-4" key = {p.id}>
+                    <div className="card mb-4 box-shadow">
+                      <img src = {p.image} height="180"/>
+                      <div className="card-body">
+                        <p className="card-text">{p.title}</p>
+                        <div className="d-flex justify-content-between align-items-center">
+                          <div className="btn-group">
+                            <button type="button" className="btn btn-sm btn-outline-secondary" onClick={ () => like(p.id) }>Like</button>
+                          </div>
+                          <small className="text-muted">{p.likes} likes</small>
+                        </div>
                       </div>
-                      <small className="text-muted">9 mins</small>
                     </div>
                   </div>
-                </div>
-              </div>
+                  )
+                }
+              )}
             </div>
+
           </div>
         </div>
   
