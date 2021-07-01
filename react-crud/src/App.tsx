0ac3411774792c4ffc47d9c00d8f53ff67c98725
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2021-07-01 22:25:18 +0300

    react, create product

--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -4,13 +4,15 @@ import {BrowserRouter, Route} from 'react-router-dom';
 
 import Products from './admin/Products';
 import Main from './main/Main';
+import ProductsCreate from './admin/ProductsCreate';
 
 function App() {
   return (
     <div className="App">
               <BrowserRouter>
                   <Route path="/" exact component = {Main}/>
-                  <Route path="/admin/products" component = {Products}/>
+                  <Route path="/admin/products" exact component = {Products}/>
+                  <Route path="/admin/products/create" exact component = {ProductsCreate}/>
               </BrowserRouter>
     </div>
   );
